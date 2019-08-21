3a897e5eabb915278ef3f8ae2aa3b71dbc9217f3
sunmeng2019 <54268525+sunmeng2019@users.noreply.github.com>
2019-08-21 21:26:06 +0800

    Update practice-1.js

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,17 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+  var c = new Array();
+for(var i = 0; i<collectionA.length; i++)
+{
+  for(var j = 0; j<collectionB.length; i++)
+  {
+    if (collectionA[i]) == collectionB[j]
+    {
+      c.push(collectionA[i]);
+      break;
+    }
+  }
+}
+return c ;
 }
